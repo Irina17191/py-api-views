311a21c7be3a782ff636c38b1d7ef60abe0ec685
Iryna Honcharenko <irynahoncharenko9@gmail.com>
2024-07-28 00:13:05 +0300

    'Solution'

--- cinema/urls.py
+++ cinema/urls.py
@@ -9,14 +9,6 @@ from cinema.views import (
     MovieViewSet,
 )
 
-# from cinema.views import movie_list, movie_detail
-
-# urlpatterns = [
-#     path("movies/", movie_list, name="movie-list"),
-#     path("movies/<int:pk>/", movie_detail, name="movie-detail"),
-# ]
-#
-# app_name = "cinema"
 
 router = routers.DefaultRouter()
 router.register("movies", MovieViewSet)
